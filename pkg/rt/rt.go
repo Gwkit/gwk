82647e2b62f0ba7bfa12650e401c8b7b0361e8d0
chen <gakiclin@gmail.com>
2017-10-31 00:04:47 +0800

    update rt configure

--- pkg/rt/rt.go
+++ pkg/rt/rt.go
@@ -1,6 +1,17 @@
 package rt
 
+import (
+	"fmt"
+	"honnef.co/go/js/dom"
+	"time"
+)
+
 type GwkRT struct {
+	canvas            *dom.HTMLCanvasElement
+	mainCanvasW       int
+	mainCanvasH       int
+	mainCanvasScale   *struct{ x, y float32 }
+	mainCanvasPostion *struct{ x, y int }
 }
 
 var rt = &GwkRT{}
@@ -18,4 +29,82 @@ func (rt *GwkRT) GetViewPort() (int, int) {
 	height := 200
 
 	return width, height
+}
+
+func (rt *GwkRT) GetMainCanvas(id string) dom.HTMLCanvasElement {
+	if rt.canvas != nil {
+		return *rt.canvas
+	}
+
+	if len(id) == 0 {
+		id = "main-canvas"
+	}
+
+	document := dom.GetWindow().Document()
+	canvas := document.GetElementByID(id).(*dom.HTMLCanvasElement)
+	if canvas == nil {
+		canvas := document.CreateElement("canvas").(*dom.HTMLCanvasElement)
+		canvas.SetID("main-canvas")
+		canvas.Style().SetProperty("zIndex", "0", "")
+		dom.GetWindow().Document().AppendChild(canvas)
+	}
+
+	rt.canvas = canvas
+
+	return *canvas
+}
+
+func (rt *GwkRT) moveMainCanvas(x, y int) {
+	canvas := rt.canvas
+
+	canvas.Style().SetProperty("position", "absolute", "")
+	canvas.Style().SetProperty("top", fmt.Sprintf("%dpx", x), "")
+	canvas.Style().SetProperty("left", fmt.Sprintf("%dpx", y), "")
+
+	rt.mainCanvasPostion.x = x
+	rt.mainCanvasPostion.y = y
+
+	return
+}
+
+func (rt *GwkRT) resizeMainCanvas(w, h, styleW, styleH int) {
+	canvas := rt.GetMainCanvas("")
+
+	canvas.Style().SetProperty("width", fmt.Sprintf("%d", w), "")
+	canvas.Style().SetProperty("height", fmt.Sprintf("%d", h), "")
+	rt.mainCanvasW = w
+	rt.mainCanvasH = h
+	rt.mainCanvasScale.x = float32(w) / float32(styleW)
+	rt.mainCanvasScale.y = float32(h) / float32(styleH)
+
+	return
+}
+
+func (rw *GwkRT) getMainCanvasScale() (float32, float32) {
+	return rt.mainCanvasScale.x, rt.mainCanvasScale.y
+}
+
+func (rt *GwkRT) getMainCanvasPosition() (int, int) {
+	return rt.mainCanvasPostion.x, rt.mainCanvasPostion.y
+}
+
+func (rt *GwkRT) createImage(src string,
+	onLoad func(*dom.HTMLImageElement),
+	onError func(interface{})) dom.HTMLImageElement {
+	image := dom.GetWindow().Document().CreateElement("image").(*dom.HTMLImageElement)
+	image.AddEventListener("onload", false, func(event dom.Event) {
+		onLoad(image)
+	})
+
+	image.AddEventListener("onerror", false, func(event dom.Event) {
+		onError(image)
+	})
+
+	image.Src = src
+
+	return *image
+}
+
+func (rt *GwkRT) requestAnimFrame(callback func(time.Duration)) {
+	dom.GetWindow().RequestAnimationFrame(callback)
 }
