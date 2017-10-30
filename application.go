8be23e81609c3e03deea186fcdb98c63cb5b6d81
chen <gakiclin@gmail.com>
2017-10-31 00:05:00 +0800

    update application configure

--- application.go
+++ application.go
@@ -1,4 +1,32 @@
 package gwk
 
+import (
+	"honnef.co/go/js/dom"
+	"github.com/Luncher/gwk/pkg/rt"
+)
+
 type Application struct {
+	win       *Window
+	view      interface{}
+	t         string
+	minHeight int
+	canvas    dom.HTMLCanvasElement
+	manager   *WindowManager
+}
+
+func NewApplication(t string) *Application {
+	app := &Application{}
+
+	app.t = t
+	app.canvas = rt.
+}
+
+func (app *Application) getView() interface{} {
+	return app.view
+}
+
+func (app *Application) setMinHeight(minHeight int) {
+	app.minHeight = minHeight
+
+	return
 }
